c6541650bd4489ff94b16b6a571662864a9ac7e5
Thomas Stöckl <thomas.stoeckl98@gmail.com>
2024-05-27 12:07:21 +0200

    addin Interface to Answer Model

--- Quiz.Core/Models/Answer.cs
+++ Quiz.Core/Models/Answer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Quiz.Core.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Quiz.Core.Models;
 
-public class Answer
+public class Answer : IAnswer
 {
     public bool IsTrue { get; set; } = false;
 
