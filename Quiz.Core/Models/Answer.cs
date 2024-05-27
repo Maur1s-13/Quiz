1c5ae1c041257bb82d683176ceb311b6c93a7de0
Thomas Stöckl <thomas.stoeckl98@gmail.com>
2024-05-27 12:03:06 +0200

    adding models answer and question, adding answer interface

---
+++ Quiz.Core/Models/Answer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.Core.Models;
+
+public class Answer
+{
+    public bool IsTrue { get; set; } = false;
+
+    public string Text { get; set; }    
+
+    public Answer(string text, bool isTrue)
+    {
+        this.Text = text;
+        this.IsTrue = isTrue;
+
+    }
+
+
+}
